cc11362f349c91ac84bd2d2b60c52396e732642c
apsolinap <38276252+apsolinap@users.noreply.github.com>
2019-02-26 02:26:32 +0800

    Update request.js
    displays pending, processing, declined, and completed
    working on adding buttons to processing, declined and completed cards

--- request.js
+++ request.js
@@ -32,37 +32,7 @@ function open_pending_tab(){
 	document.getElementById("processing_div").style.display="none";
 	document.getElementById("declined_div").style.display="none";
 
-	//connect to db and get pending requests based on laboratory
-	//assume lab =  chemistry for now
-	var lab = "chemistry";
-	var database = firebase.database().ref(lab + "/request");
-	
-	//display pending requests
-	database.on('child_added',function(snapshot){
-		var data = snapshotToArray(snapshot);
-		console.log(data)
-		var status = data[6];
-		if(status.toLowerCase() == "pending"){
-			var items = data[1];
-			var requestId = data[3];
-			var requestNeeded = data[4];
-			var requestSent = data[5];
-			var requestorId = data[7];
-			var itemStorage = '';
-			for (var i = 0;i<items.length;i++){
-				//limit to three
-				if(i < 3){
-					itemStorage += '<br>Item: '+ i+'<br> item id: '+data[1][i]["itemID"]+'<br>amount: '+data[1][i]["amount"]+'<br>quantity: '+data[1][i]["quantity"]+'<br>unit '+ data[1][i]["unit"] +'<br>';
-				}
-				else{
-					break;
-				}
-
-			}
-			var container = document.getElementById('items-div-pcd');
-			container.innerHTML += '<div class="card-div-pending rounded" id="'+requestId+'"onclick="seeMoreRequest(this)"><span class="card status">'+ status + '</span><span class="card request-number">Request Number: '+requestId+'</span><span class="card request-sent">Request Sent:'+requestSent+'</span><span class="card request-needed">Request Needed:'+requestNeeded+'</span><span class="card request-requestor">Requestor Name:'+requestorId+'</span><span class="card request-items">Items:'+itemStorage+'</span><button type="button" class="btn btn-info btn card-btn" id="declineMain"> Decline </button><button class="btn btn card-btn" id="'+requestId+'" onclick="showConfirmModal(this)" id="reqAdd"> Process </button></div>';
-		}
-	});
+	getRequests("pending");
 }
 
 function open_processing_tab(){
@@ -79,6 +49,7 @@ function open_processing_tab(){
 	document.getElementById("pending_div").style.display="none";
 	document.getElementById("completed_div").style.display="none";
 	document.getElementById("declined_div").style.display="none";
+	getRequests("others");
 }
 
 function open_declined_tab(){
@@ -90,6 +61,7 @@ function open_declined_tab(){
 	document.getElementById("completed_div").style.display="none";
 	document.getElementById("processing_div").style.display="none";
 	document.getElementById("declined_div").style.display="block";
+	getRequests("declined");
 
 }
 
@@ -102,8 +74,136 @@ function open_completed_tab(){
 	document.getElementById("pending_div").style.display="none";
 	document.getElementById("processing_div").style.display="none";
 	document.getElementById("declined_div").style.display="none";
-
+	getRequests("complete");
 }
+
+
+function getRequests(status){
+	//connect to db and get pending requests based on laboratory
+	//assume lab =  chemistry for now
+	var lab = "chemistry";
+	var database = firebase.database().ref(lab + "/request");
+	
+	//display requests based on parameter "status"
+	database.on('child_added',function(snapshot){
+		var data = snapshotToArray(snapshot);
+		console.log(data)
+		var requestStatus = data[6];
+		switch(status){
+			case "pending":
+				if(requestStatus.toLowerCase() == "pending"){
+					var items = data[1];
+					var requestId = data[3];
+					var requestNeeded = data[4];
+					var requestSent = data[5];
+					var requestorId = data[7];
+					var itemStorage = '';
+					for (var i = 0;i<items.length;i++){
+						//limit item display to three
+						if(i < 3){
+							itemStorage += '<br>Item: '+ i+'<br> item id: '+data[1][i]["itemID"]+'<br>amount: '+data[1][i]["amount"]+'<br>quantity: '+data[1][i]["quantity"]+'<br>unit '+ data[1][i]["unit"] +'<br>';
+						}
+						else{
+							break;
+						}
+					} 
+
+					var container = document.getElementById('pending-items-div-pcd');
+					container.innerHTML += '<div class="card-div-pending rounded" id="'+requestId+'" ondblclick="seeMoreRequest(this)"><span class="card status">'+ requestStatus + '</span><span class="card request-number">Request Number: '+requestId+'</span><span class="card request-sent">Request Sent:'+requestSent+'</span><span class="card request-needed">Request Needed:'+requestNeeded+'</span><span class="card request-requestor">Requestor Name:'+requestorId+'</span><span class="card request-items">Items:'+itemStorage+'</span><button type="button" class="btn btn-info btn card-btn" id="declineMain"> Decline </button><button class="btn btn card-btn" id="'+requestId+'" onclick="showConfirmModal(this)" id="reqAdd"> Process </button></div>';
+				}
+			case "declined":
+				if(requestStatus.toLowerCase() == "declined" ){
+					var items = data[1];
+					var requestId = data[3];
+					var requestNeeded = data[4];
+					var requestSent = data[5];
+					var requestorId = data[7];
+					var itemStorage = '';
+					for (var i = 0;i<items.length;i++){
+						//limit item display to three
+						if(i < 3){
+							itemStorage += '<br>Item: '+ i+'<br> item id: '+data[1][i]["itemID"]+'<br>amount: '+data[1][i]["amount"]+'<br>quantity: '+data[1][i]["quantity"]+'<br>unit '+ data[1][i]["unit"] +'<br>';
+						}
+						else{
+							break;
+						}
+					} 
+				var container = document.getElementById('declined-items-div-pcd');
+					container.innerHTML += '<div class="card-div-pending rounded" id="'+requestId+'" ondblclick="seeMoreRequest(this)"><span class="card status">'+ requestStatus + '</span><span class="card request-number">Request Number: '+requestId+'</span><span class="card request-sent">Request Sent:'+requestSent+'</span><span class="card request-needed">Request Needed:'+requestNeeded+'</span><span class="card request-requestor">Requestor Name:'+requestorId+'</span><span class="card request-items">Items:'+itemStorage+'</span></div>';
+				
+					//var buttons = '<button class="btn btn card-btn" id="'+requestId+'" onclick="showConfirmModal(this)" id="reqAdd"> Update </button>';
+				}
+			case "complete":
+				if(requestStatus.toLowerCase() == "complete" ){
+					var items = data[1];
+					var requestId = data[3];
+					var requestNeeded = data[4];
+					var requestSent = data[5];
+					var requestorId = data[7];
+					var itemStorage = '';
+					for (var i = 0;i<items.length;i++){
+						//limit item display to three
+						if(i < 3){
+							itemStorage += '<br>Item: '+ i+'<br> item id: '+data[1][i]["itemID"]+'<br>amount: '+data[1][i]["amount"]+'<br>quantity: '+data[1][i]["quantity"]+'<br>unit '+ data[1][i]["unit"] +'<br>';
+						}
+						else{
+							break;
+						}
+					} 
+				var container = document.getElementById('completed-items-div-pcd');
+					container.innerHTML += '<div class="card-div-pending rounded" id="'+requestId+'" ondblclick="seeMoreRequest(this)"><span class="card status">'+ requestStatus + '</span><span class="card request-number">Request Number: '+requestId+'</span><span class="card request-sent">Request Sent:'+requestSent+'</span><span class="card request-needed">Request Needed:'+requestNeeded+'</span><span class="card request-requestor">Requestor Name:'+requestorId+'</span><span class="card request-items">Items:'+itemStorage+'</span></div>';
+				
+					//var buttons = '<button class="btn btn card-btn" id="'+requestId+'" onclick="showConfirmModal(this)" id="reqAdd"> Update </button>';
+				}
+			default:
+				//display processing cards
+				var items = data[1];
+					var requestId = data[3];
+					var requestNeeded = data[4];
+					var requestSent = data[5];
+					var requestorId = data[7];
+					var itemStorage = '';
+					for (var i = 0;i<items.length;i++){
+						//limit item display to three
+						if(i < 3){
+							itemStorage += '<br>Item: '+ i+'<br> item id: '+data[1][i]["itemID"]+'<br>amount: '+data[1][i]["amount"]+'<br>quantity: '+data[1][i]["quantity"]+'<br>unit '+ data[1][i]["unit"] +'<br>';
+						}
+						else{
+							break;
+						}
+					} 
+				var container = document.getElementById('processing-items-div-pcd');
+					container.innerHTML += '<div class="card-div-pending rounded" id="'+requestId+'" ondblclick="seeMoreRequest(this)"><span class="card status">'+ requestStatus + '</span><span class="card request-number">Request Number: '+requestId+'</span><span class="card request-sent">Request Sent:'+requestSent+'</span><span class="card request-needed">Request Needed:'+requestNeeded+'</span><span class="card request-requestor">Requestor Name:'+requestorId+'</span><span class="card request-items">Items:'+itemStorage+'</span></div>';
+				
+					//var buttons = '<button class="btn btn card-btn" id="'+requestId+'" onclick="showConfirmModal(this)" id="reqAdd"> Update </button>';
+				
+				
+
+
+		}
+		/*if(requestStatus.toLowerCase() == "pending"){
+			var items = data[1];
+			var requestId = data[3];
+			var requestNeeded = data[4];
+			var requestSent = data[5];
+			var requestorId = data[7];
+			var itemStorage = '';
+			for (var i = 0;i<items.length;i++){
+				//limit to three
+				if(i < 3){
+					itemStorage += '<br>Item: '+ i+'<br> item id: '+data[1][i]["itemID"]+'<br>amount: '+data[1][i]["amount"]+'<br>quantity: '+data[1][i]["quantity"]+'<br>unit '+ data[1][i]["unit"] +'<br>';
+				}
+				else{
+					break;
+				}
+
+			}
+			
+		}*/
+	});
+}
+
+
 
 //double clicking to see more of the request
 function seeMoreRequest(request){
@@ -138,18 +238,22 @@ function seeMoreRequest(request){
 //show confirm modal
 function showConfirmModal(req){
 	var requestId = req.id;
+	document.getElementById("confirm-text").innerHTML = "Update Status";
 	document.getElementById("confirmStatusChange").setAttribute('name', requestId);
 	document.getElementById("confirmStatusChange").setAttribute('onclick','changeStatusOfRequest(this)')
 	$('#confirmModal').modal('show');
 }
 //change status of request from pending to processing
 function changeStatusOfRequest(req){
-	console.log(req.name + " is now processing.");
+	//console.log(req.name + " is now processing.");
+	//assuming lab = chemistry
 	var lab = "chemistry";
 	var database = firebase.database().ref(lab);
 	
 	database.child("/request").orderByChild("requestID").equalTo(req.name).once("value").then(function(snapshot){
 			snapshot.forEach(function(childSnapshot) {
+					var updateThisChild = childSnapshot.key;
+					
 					console.log(childSnapshot.val().requestID);
 					var details = childSnapshot.val().details;
 					var requestID = childSnapshot.val().requestID;
@@ -158,16 +262,20 @@ function changeStatusOfRequest(req){
 					var requestNeeded = childSnapshot.val().requestNeeded;
 					var requestSent = childSnapshot.val().requestSent;
 					var userID = childSnapshot.val().userID;
-					var data = {
-						details: details,
-						requestID: requestID,
-						items: items;
-						proxyID: proxyID,
-						requestNeeded: requestNeeded,
-						requestSent: requestSent,
-						userID: userID,
-						status: "Processing"
+					var newData = {
+						"details": details,
+						"requestID": requestID,
+						"items": items,
+						"proxyID": proxyID,
+						"requestNeeded": requestNeeded,
+						"requestSent": requestSent,
+						"userID": userID,
+						"status": "preparing"
 					};
+					//console.log(updateThisChild);
+					var updates = {}
+					updates[""+lab+"/request/"+ updateThisChild] = newData;
+					firebase.database().ref().update(updates);
 			});
 	})
 }
